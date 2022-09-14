943bad9db62b82a200eec77e565afd48420af37d
MarkusMaal2 <markus.maal@voco.ee>
2022-09-14 11:32:43 +0300

    Task 1.1 solution

--- variables_datatypes/app.js
+++ variables_datatypes/app.js
@@ -1,12 +1,7 @@
 /**
- * This is a documentation comment
+ * Data types
+ *
+ * primitives - integer, float, String
  */
 
-// this is a comment on a single line
-
-/* this is a multiline comment
- * hello! */
-
 console.log("Tere, maailm!");
-console.log("Teine!");
-console.log("Kolmas!");
