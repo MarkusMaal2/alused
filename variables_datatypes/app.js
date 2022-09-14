e823367c1b524a5956757b83435ee6e8c4f4996e
MarkusMaal2 <markus.maal@voco.ee>
2022-09-14 10:57:22 +0300

    Text output and comments in app.js

---
+++ variables_datatypes/app.js
@@ -0,0 +1,12 @@
+/**
+ * This is a documentation comment
+ */
+
+// this is a comment on a single line
+
+/* this is a multiline comment
+ * hello! */
+
+console.log("Tere, maailm!");
+console.log("Teine!");
+console.log("Kolmas!");
