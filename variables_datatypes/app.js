ee93ed34f01b30980b870dd148ecef85e320afa8
MarkusMaal2 <markus.maal@voco.ee>
2022-09-14 11:42:07 +0300

    Task 1.2 solution

--- variables_datatypes/app.js
+++ variables_datatypes/app.js
@@ -2,6 +2,15 @@
  * Data types
  *
  * primitives - integer, float, String
+ * objects
+ *
+ * variables:
+ * let variableName = value; // mutable
+ * const variableName = value; //immutable
  */
 
-console.log("Tere, maailm!");
+const aasta = 2021;
+const president = "Alar Karis";
+const lause_keskosa = ". aastal valiti Eesti presidendiks";
+const lause = String(aasta) + lause_keskosa + " " + president;
+console.log(lause);
