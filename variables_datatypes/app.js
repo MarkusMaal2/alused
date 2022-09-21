a37b322e611195665e6e26ba9d61a27ac6b8f477
MarkusMaal2 <markus.maal@voco.ee>
2022-09-21 11:32:23 +0300

    Task 2.1 solution

--- variables_datatypes/app.js
+++ variables_datatypes/app.js
@@ -1,33 +1,7 @@
-/**
- * Data types
- *
- * primitives - integer, float, String
- * objects
- *
- * variables:
- * let variableName = value; // mutable
- * const variableName = value; //immutable
- */
-
-// Task 1.6
-
-// Number of people and seats per bus
-const humanCount = 70;
-const busSeats = 24;
-
-// Number of buses (Math.floor function rounds down the division)
-const busCount = Math.floor(humanCount / busSeats);
-
-// The number of travellers, who were not able to make it into the bus
-const abandonedCount = humanCount % busSeats;
-
-// The number of travellers, who were able to make it into the bus
-const travellingCount = humanCount - abandonedCount;
-
-// Output the results
-console.log("Kokku on " + humanCount + " reisjat.");
-console.log("Ühes bussis on " + busSeats + " kohta.");
-console.log("Kokku saabus " + busCount + " bussi.");
-console.log("Bussidesse mahtus " + travellingCount + " inimest.");
-console.log("Bussidest jäi maha " + abandonedCount + " inimest.");
-
+// 2.1
+let temperatuur = Number(prompt("Sisesta õhutemperatuur: "))
+if (temperatuur > 4.0) {
+    console.log("Ei ole jäätumise ohtu")
+} else {
+    console.log("On jäätumise oht")
+}
