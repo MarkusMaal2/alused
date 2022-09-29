ce49934bd9f96f78f2a2c6e4a42bbf125914921e
MarkusMaal2 <markus.maal@voco.ee>
2022-09-29 12:47:18 +0300

    Algorithms, Task 3.2 alternate solution

--- algorithms/app.js
+++ algorithms/app.js
@@ -1,12 +1,15 @@
 // 3.2 Jänesevanemate mure (while tsükliga)
 let lapCount = Number(prompt("Sisesta ringide arv: "))
 let carrots = 0
-let increment = 2
+let increment = 0
 let lap = 0
-while (lap < lapCount - 1) {
-    carrots += increment
-    //console.log("+" + increment)
-    increment += 2
-    lap += 2
+while (lap <= lapCount) {
+    //console.log(`${lap}. ring`)
+    if (lap % 2 === 0) {
+        carrots += increment
+        //console.log(`+${increment} porgandit`)
+        increment += 2
+        lap += 2
+    }
 }
-console.log("Porgandite koguarv on " + carrots + ".")
+console.log(`Porgandite koguarv on ${carrots}.`)
