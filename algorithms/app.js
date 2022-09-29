5cf306ad9753cc4c4b965f5c384ba5d9e44bdc69
MarkusMaal2 <markus.maal@voco.ee>
2022-09-29 12:17:14 +0300

    Algorithms, Task 3.2 solution

--- algorithms/app.js
+++ algorithms/app.js
@@ -1,6 +1,10 @@
-// 3.1 Äratus
-loopCount = Number(prompt("Sisestage mitu korda äratada: "))
-
-for (let i = 0; i < loopCount; i++) {
-    console.log("Tõuse ja sära!")
+// 3.2 Jänesevanemate mure
+lapCount = Number(prompt("Sisesta ringide arv: "))
+carrots = 0;
+increment = 2;
+for (let l = 0; l < lapCount - 1; l += 2) {
+    carrots += increment;
+    console.log("+" + increment)
+    increment += 2;
 }
+console.log("Porgandite koguarv on " + carrots + ".")
