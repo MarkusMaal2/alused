c4c97cda69ef6f91197f39e624a28d93dcf144aa
MarkusMaal2 <markus.maal@voco.ee>
2022-09-29 13:34:15 +0300

    Algorithms, Task 3.4

--- algorithms/app.js
+++ algorithms/app.js
@@ -1,15 +1,10 @@
-// 3.2 Jänesevanemate mure (while tsükliga)
-let lapCount = Number(prompt("Sisesta ringide arv: "))
-let carrots = 0
-let increment = 0
-let lap = 0
-while (lap <= lapCount) {
-    //console.log(`${lap}. ring`)
-    if (lap % 2 === 0) {
-        carrots += increment
-        //console.log(`+${increment} porgandit`)
-        increment += 2
-        lap += 2
-    }
+// 3.4 Õunte jagamine
+let apples = 14
+let dwarfCount = Number(prompt("Mitu pöialpoissi tahab õunu? "))
+
+for (let d = 0; d < dwarfCount; d++) {
+    let giveApple = Math.ceil(Math.random() * 2)
+    apples -= giveApple
+    console.log(giveApple)
 }
-console.log(`Porgandite koguarv on ${carrots}.`)
+console.log(`Lumivalgekesele jäi ${apples}`)
